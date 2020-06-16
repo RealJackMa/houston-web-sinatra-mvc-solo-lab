eb4c6937c1cb309e17c44a4872d36bdd5f82ac02
CelestialEmperor <mapeiyao@gmail.com>
2020-06-16 21:25:39 +0100

    Added Cool Stuff.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,62 @@
 class ApplicationController < Sinatra::Base
-    configure do
-        set :views, 'app/views'
-        set :method_override, true
-      end
-    
-      get '/' do
-        erb :welcome
-      end
 
+    set :views, 'app/views'
+    set :method_override, true
+    set :public_folder, 'stuff'
+
+    get '/' do
+      erb :home
+    end
+  
+    get '/home' do
+      erb :home
+    end
+
+   # display list of all the puppies
+     get '/puppies' do
+        @puppies = Puppy.all
+        erb :index
+    end
+
+    # Form for creating a puppy
+    get '/puppies/new' do 
+        erb :new
+    end
+
+    # display single puppy info
+    get '/puppies/:id' do 
+        @puppy = find_puppy(params)
+        erb :show
+    end
+
+    # create a puppy
+    post '/puppies' do
+        puppy = Puppy.create(params)
+        redirect "/puppies"
+    end
+
+    # form for updating a puppy
+    get '/puppies/:id/edit' do
+        @puppy = find_puppy(params)
+        erb :edit
+    end
+
+    # update a puppy
+    patch '/puppies/:id' do
+        puppy = find_puppy(params)
+        puppy.update(name: params[:name], breed:params[:breed])
+
+        redirect "/puppies/#{puppy.id}"
+    end
+
+    delete '/puppies/:id' do 
+        puppy = find_puppy(params)
+        puppy.destroy
+        redirect "/puppies"
+    end
+
+    def find_puppy(params)
+        Puppy.find(params[:id])
+    end
    
 end
--- app/controllers/puppies_controller.rb
+++
@@ -1,3 +0,0 @@
-class PuppiesController < ApplicationController
-   
-end
